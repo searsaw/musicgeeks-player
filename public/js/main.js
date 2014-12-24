3a9039109ecb52f185845ef86efa872c69a83471
Alex Sears <alexwsears@gmail.com>
2014-12-24 13:00:19 -0500

    Finished tablet and desktop version

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,31 @@
 (function($){
+  var artistDropdownBtn = $('#artist-dropdown-btn'),
+    artistsContainer = $('#artists-container'),
+    docHeight = window.innerHeight,
+    iArrow = $('#artist-dropdown-btn i'),
+    soundcloudSong = $('#soundcloud-song'),
+    footer = $('footer');
 
-  $('#artist-dropdown-btn').on('click', function() {
-    var that = $(this),
-        artistsContainer = $('#artists-container'),
-        docHeight = window.innerHeight,
-        top = that.offset().top,
-        eleHeight = that.height(),
-        i = $('#artist-dropdown-btn i');
+  function setArtistContainerHeight() {
+    if (artistDropdownBtn.is(':visible')) {
+      artistsContainer.height(docHeight - artistDropdownBtn.offset().top - artistDropdownBtn.height());
+    } else {
+      artistsContainer.height(docHeight - soundcloudSong.height() - footer.height());
+    }
+  }
 
+  artistDropdownBtn.on('click', function() {
     if (artistsContainer.is(':visible')) {
       artistsContainer.slideUp();
-      i.detach().appendTo(that);
-      i.removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
+      iArrow.detach().appendTo(artistDropdownBtn);
+      iArrow.removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
     } else {
-      artistsContainer.height(docHeight - top - eleHeight);
       artistsContainer.slideDown();
-      i.detach().prependTo(that);
-      i.removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
+      iArrow.detach().prependTo(artistDropdownBtn);
+      iArrow.removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
     }
-
   });
+
+  setArtistContainerHeight();
 
 })(jQuery);
