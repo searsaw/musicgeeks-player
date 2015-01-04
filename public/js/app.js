2b2c9a292c56132daa35454463d81de3e403d014
Alex Sears <alexwsears@gmail.com>
2015-01-04 16:58:59 -0500

    Moved height JS into app.js

--- public/js/app.js
+++ public/js/app.js
@@ -17,8 +17,34 @@ angular.module('MusicgeeksApp', ['ngResource', 'ngRoute'])
     return $resource('http://musicgeeksapi.alexsears.com/v1/songs', { apikey: 'F7284ACC3C51E' });
   })
   .controller('MusicgeeksController', function($scope, Song) {
+    var artistDropdownBtn = $('#artist-dropdown-btn'),
+        artistsContainer = $('#artists-container'),
+        docHeight = window.innerHeight,
+        iArrow = $('#artist-dropdown-btn i'),
+        soundcloudSong = $('#soundcloud-song'),
+        footer = $('footer');
+
+    artistDropdownBtn.on('click', function() {
+      if (artistsContainer.is(':visible')) {
+        artistsContainer.slideUp();
+        iArrow.detach().appendTo(artistDropdownBtn);
+        iArrow.removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
+      } else {
+        artistsContainer.slideDown();
+        iArrow.detach().prependTo(artistDropdownBtn);
+        iArrow.removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
+      }
+    });
+
     $scope.songs = Song.query(function() {
       $scope.current_song = $scope.songs[0];
+
+      // set artist container height
+      if (artistDropdownBtn.is(':visible')) {
+        artistsContainer.height(docHeight - artistDropdownBtn.offset().top - artistDropdownBtn.height());
+      } else {
+        artistsContainer.height(docHeight - soundcloudSong.height() - footer.height());
+      }
     });
 
     $scope.play = function(song) {
