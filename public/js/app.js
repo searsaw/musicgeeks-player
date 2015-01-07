a58cab4639d749734a68a5b99e00edbf2b1e7951
Alex Sears <alexwsears@gmail.com>
2015-01-06 23:32:24 -0500

    Cleaned up some code. Soundcloud API used to interact with player. Player goes to next song when song ends

--- public/js/app.js
+++ public/js/app.js
@@ -24,12 +24,38 @@ angular.module('MusicgeeksApp', ['ngResource', 'ngRoute'])
   .controller('MusicgeeksController', function($scope, Song) {
     var artistDropdownBtn = $('#artist-dropdown-btn'),
         artistsContainer = $('#artists-container'),
-        docHeight = window.innerHeight,
         iArrow = $('#artist-dropdown-btn i'),
-        soundcloudSong = $('#soundcloud-song'),
-        footer = $('footer');
+        soundcloudIframe = document.getElementById('soundcloud-song'),
+        soundcloudWidget = {},
+        footer = $('footer'),
+        songIndex = 0;
 
-    artistDropdownBtn.on('click', function() {
+    $scope.songs = Song.query(function() {
+      $scope.current_song = $scope.songs[0];
+
+      // set artist container height
+      if (artistDropdownBtn.is(':visible')) {
+        artistsContainer.height(window.innerHeight - artistDropdownBtn.offset().top - artistDropdownBtn.height());
+      } else {
+        artistsContainer.height(window.innerHeight - soundcloudIframe.height - footer.height());
+      }
+
+      soundcloudIframe.src = "https://w.soundcloud.com/player/?url=" + $scope.current_song.soundcloud_url;
+      soundcloudWidget = SC.Widget(soundcloudIframe);
+      soundcloudWidget.bind(SC.Widget.Events.READY, function() {
+        soundcloudWidget.bind(SC.Widget.Events.FINISH, function() {
+          // forces angular to see update since soundcould api
+          // updates happen outside angular's digest loop
+          $scope.$apply(function() {
+            $scope.play($scope.songs[++songIndex], false);
+          });
+        });
+      });
+
+      $scope.play($scope.current_song, false);
+    });
+
+    $scope.toggleArtists = function() {
       if (artistsContainer.is(':visible')) {
         artistsContainer.slideUp();
         iArrow.detach().appendTo(artistDropdownBtn);
@@ -39,21 +65,19 @@ angular.module('MusicgeeksApp', ['ngResource', 'ngRoute'])
         iArrow.detach().prependTo(artistDropdownBtn);
         iArrow.removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
       }
-    });
+    };
 
-    $scope.songs = Song.query(function() {
-      $scope.current_song = $scope.songs[0];
-
-      // set artist container height
-      if (artistDropdownBtn.is(':visible')) {
-        artistsContainer.height(docHeight - artistDropdownBtn.offset().top - artistDropdownBtn.height());
-      } else {
-        artistsContainer.height(docHeight - soundcloudSong.height() - footer.height());
-      }
-    });
-
-    $scope.play = function(song) {
+    $scope.play = function(song, update_index) {
       $scope.current_song = song;
+      soundcloudWidget.load(song.soundcloud_url, {
+        auto_play: true,
+        color: '777',
+        hide_related: false,
+        show_reposts: false
+      });
+
       ga('send', 'event', song.page_title, 'click', 'play song');
+
+      if (update_index) songIndex = $scope.songs.indexOf(song);
     };
   });
